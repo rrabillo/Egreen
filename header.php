348faca5dbf1718283c0066f79dc09feff178625
Richard <richard.rabillon@live.fr>
2016-11-15 17:41:31 +0100

    oklm

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>E-Green</title>
+  <meta name="description" content="E-Green event">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="./css/style.css?v=1.0">
+  <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+  <!--[if lt IE 9]>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,151 @@
+<?php include('./header.php'); ?>
+	<header>
+		<section id="video-container">
+			<video  src="./videos/bg1.webm" type="video/webm" autoplay="true" loop="true"></video>
+		</section>
+		<nav class="navbar navbar-default navbar-fixed-top clearfix">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sept-main-nav" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><img src="./imgs/egreen_logo.png" alt="logo E-Green" class="img-responsive"></a>
+				</div>
+				<div class="navbar-collapse main-nav collapse" id="sept-main-nav" aria-expanded="false" style="height: 0px;">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="#home">Home</a></li>
+						<li><a href="#about">E-Green</a></li>
+						<li><a href="#service">La battle énergie</a></li>
+						<li><a href="#portfolio">Newsletter</a></li>
+						<li><a href="#pricing">Ils ont joué le jeu</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+		<h1>LUTTONS CONTRE LE GASPILLAGE ÉNERGÉTIQUE</h1>
+	</header>
+	<main>
+		<div class="container">
+			<section class="row" id="e-green-intro">
+				<div class="col-lg-12">
+					<div class="heading-img">
+						<h2>E-Green c'est quoi ? </h2>
+							<img src="./imgs/egreen_logo.png" alt="logo E-Green" class="img-responsive">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12 col-md-offset-2 vignette-intro">
+					<div class="picto-intro">
+						<div class="img-rounded">
+							<img src="./imgs/power-button.png" alt="Power button pictogram">
+						</div>
+						<h3>Un dispositif...</h3>
+						<p>
+							Qui aide à réduire les consommations d'énergie dans tout type de bâtiments.
+						</p>	
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12 vignette-intro">
+					<div class=" picto-intro">
+						<div class="img-rounded">
+							<img src="./imgs/capteur.png" alt="Capteur pictogram">
+						</div>
+						<h3>Qui permet de...</h3>
+						<p>
+							Mieux comprendre les consommations grâce à des capteurs
+							qui mesurent en temps réel les consommations d'électricité, d'eau, de gaz
+							ou la température intérieure.
+						</p>	
+					</div>
+				</div>
+			</section>
+			<section class="row" id="battle-energie">
+				<div class="col-md-12">
+					<h2>LA BATTLE ÉNERGIE</h2>
+					<div class="content-battle">
+						<div class="col-md-6 col-sm-12">
+							<h3>Un projet en commun pour réduire les consommations d'énergie</h3>
+							<p>
+								Le mois de décembre arrive, le froid commence à s’installer.
+								 C’est aussi un des mois où la consommation d’énergie dans l’ensemble des bâtiments est la
+								plus importante.
+								E-Green et le collectif le Sentier se mobilisent pour lutter contre le gaspillage énergétique.
+								Selon de nombreuses études, Il faut 21 jours pour passer le premier palier de la création d’une
+								habitude. Une occasion ludique et décalée pour découvrir des micro-gestes du quotidien et
+								d’adopter de nouvelles habitudes plus durables et respectueuses de l’environnement.
+							</p>
+						</div>
+						<div class="col-md-6 col-sm-12">
+							<img src="./imgs/e-green-sentier.png" alt="E-Green et le Sentier">	
+						</div>
+					</div>
+					<div class="content-battle-second">
+						<div class="col-md-4 col-sm-12 number-battle">
+							<div>
+								<span class="number">
+									21 jours
+								</span>
+								<p>
+									Entre plusieurs bâtiments du quartier
+								</p>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-12 number-battle">
+							<div>
+								<span class="number">
+									1
+								</span>
+								<p>
+									Battle Énergie
+								</p>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-12 number-battle">
+							<div>
+								<span class="number">
+									15%
+								</span>
+								<p>
+									Objectif de réduction sur la facture d'énergie pour chaque bâtiment
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="row" id="le-defi">
+				<div class="col-md-12">
+					<h2>Le défi</h2>
+					<div class="content-defi">
+						<div class="col-md-6 col-sm-12">
+							<h3>Un projet en commun pour réduire les consommations d'énergie</h3>
+							<p>
+								Le mois de décembre arrive, le froid commence à s’installer.
+								 C’est aussi un des mois où la consommation d’énergie dans l’ensemble des bâtiments est la
+								plus importante.
+								E-Green et le collectif le Sentier se mobilisent pour lutter contre le gaspillage énergétique.
+								Selon de nombreuses études, Il faut 21 jours pour passer le premier palier de la création d’une
+								habitude. Une occasion ludique et décalée pour découvrir des micro-gestes du quotidien et
+								d’adopter de nouvelles habitudes plus durables et respectueuses de l’environnement.
+							</p>
+						</div>
+						<div class="col-md-6 col-sm-12">
+							<h3>Un projet en commun pour réduire les consommations d'énergie</h3>
+							<p>
+								Le mois de décembre arrive, le froid commence à s’installer.
+								 C’est aussi un des mois où la consommation d’énergie dans l’ensemble des bâtiments est la
+								plus importante.
+								E-Green et le collectif le Sentier se mobilisent pour lutter contre le gaspillage énergétique.
+								Selon de nombreuses études, Il faut 21 jours pour passer le premier palier de la création d’une
+								habitude. Une occasion ludique et décalée pour découvrir des micro-gestes du quotidien et
+								d’adopter de nouvelles habitudes plus durables et respectueuses de l’environnement.
+							</p>	
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+<?php include('./footer.php'); ?>
